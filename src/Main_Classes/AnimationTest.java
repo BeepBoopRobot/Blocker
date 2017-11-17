76fd6f3b8f66985146606be88b7ecfe7326a3014
beepbooprobot <benlangford10@gmail.com>
2017-11-17 22:31:55 +0000

    Building the main functions

--- src/Main_Classes/AnimationTest.java
+++ src/Main_Classes/AnimationTest.java
@@ -2,11 +2,19 @@ package Main_Classes;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class AnimationTest {
-    public AnimationTimer at;
+    public static AnimationTimer at;
 
     public static void main(String[] args) {
         new JFXPanel();
@@ -17,10 +25,38 @@ public class AnimationTest {
     private static void Launch() {
         Stage stage = new Stage();
         stage.setTitle("I actually wanna fuckin die");
+        stage.setWidth(1000);
+        stage.setHeight(1000);
+        stage.setResizable(false);
+        stage.show();
 
+        Group group = new Group();
+        Scene scene = new Scene(group);
+        Canvas canvas = new Canvas();
+        canvas.setWidth(1000);
+        canvas.setHeight(1000);
+        group.getChildren().add(canvas);
+        stage.setScene(scene);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Image image = new Image("images/skeleton-animated-gif-20.gif");
+        ImageView iv = new ImageView(image);
+        gc.drawImage(image, 100, 100);
+
+        Animations a = new Animations();
+
+        AnimationTimer at = a.bone(gc, image);
+        at.start();
     }
+
 
     private static void Control() {
         Stage stage = new Stage();
+        stage.setTitle("Controller");
+        stage.setWidth(323);
+        stage.setHeight(200);
+        stage.setResizable(false);
+        stage.show();
     }
 }
--- src/Main_Classes/Animations.java
+++ src/Main_Classes/Animations.java
@@ -1,4 +1,22 @@
 package Main_Classes;
 
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
 public class Animations {
+    public static AnimationTimer at;
+    public AnimationTimer bone(GraphicsContext gc, Image image) {
+
+        at = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                gc.setFill(Color.CYAN);
+                gc.fillRect(0, 0, 1000, 1000);
+                gc.drawImage(image, 200, 200);
+            }
+        };
+        return at;
+    }
 }
